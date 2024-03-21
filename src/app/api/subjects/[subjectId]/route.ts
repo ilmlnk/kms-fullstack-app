9026e92c1d8018d4660fab0d24d22d13cdd4b0c8
ilmlnk <ilmlnkcorp@gmail.com>
2024-03-21 23:40:23 +0100

    routes config

--- src/app/api/subjects/[subjectId]/route.ts
+++ src/app/api/subjects/[subjectId]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function PATCH(
     req: Request,
-    { params }: { params: { subjectId: string } }
+    { params }: { params: { subjectId: number } }
 ) {
     try {
         const { subjectId } = params;
---
+++ src/app/api/teacher/route.ts
@@ -0,0 +1,15 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+    req: Request
+    ) {
+    try {
+        const teachers = await db.teacher.findMany();
+        return NextResponse.json(teachers);
+    } catch (error) {
+        console.log('[TEACHERS]', error);
+        return new NextResponse("Internal Error", { status: 500 });
+    } 
+}
+
